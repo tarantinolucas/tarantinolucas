f8a2b30e4405f10e8302574aacc16e9f452bfbbd
Lucas <71748804+tarantinolucas@users.noreply.github.com>
2022-11-08 16:42:21 -0300

    Commit#21
    Se realizan cambios generales:
      - Justificados de textos
      - Actualizacion de la portada
      - Cambio del logo de menu hamburguesa y el la bandera de cambio de idioma
      - Actualizacion de leyenda de producto

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-const $form = document.querySelector("#form");
-const $buttonSubmit = document.querySelector("#buttonSubmit");
-
-$form.addEventListener("submit", submitManual);
-
-function submitManual(event) {
-  event.preventDefault();
-  const form = new FormData(this);  
-  $buttonSubmit.setAttribute(
-    "href",
-    `mailto:info@eym.com.ar?subject=${form.get("name")} ${form.get(
-      "email"
-    )} ${form.get("phone")} ${form.get("tipo-de-contacto")}&body=${form.get(
-      "message"
-    )}`
-  );
-  $buttonSubmit.click();
+function enviarTexto() {
+    let texto=document.getElementById("form-email-input").value;
+    document.getElementById("hidden-email").value=texto;
 }
